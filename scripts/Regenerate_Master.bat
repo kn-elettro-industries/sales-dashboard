2257b9a16645426caa69b34408f4665c2c5f0354
Aditya Singh <adityasingh3499@gmail.com>
2026-03-12 17:12:44 +0530

    chore: remove deprecated debug and setup scripts

--- scripts/Regenerate_Master.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo --- Regenerating Sales Master Excel ---
-echo.
-echo NOTE: Please ensure 'data/output/sales_master.xlsx' is CLOSED before running this.
-echo.
-".venv\Scripts\python.exe" scripts\regenerate_excel.py
-echo.
-pause
